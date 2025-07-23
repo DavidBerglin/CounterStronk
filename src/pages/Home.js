5687df52d5d127093ef75bb7328a248c4b8beb4c
DavidBerglin <david.berglin@gmail.com>
2025-07-23 11:11:01 +0200

    dddd

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,11 @@
 import { Link } from "react-router-dom";
-import "./Home.css"
 
-function Home() {
+export default function Home() {
   return (
-    <div className="home">
-      <div className="hero">
-        <h1>CounterStronk</h1>
-        <p>CS2's holy dumpster of skins, tierlists and opinions you never asked for.</p>
-        <Link to="/blog" className="hero-button">🔥 See our latest roasts</Link>
-      </div>
+    <div style={{ backgroundColor: "black", color: "lime", height: "100vh", textAlign: "center", paddingTop: "5rem" }}>
+      <h1>CounterStronk</h1>
+      <p>CS2's holy dumpster of skins, tierlists and opinions you never asked for.</p>
+      <Link to="/blog" style={{ color: "yellow", fontSize: "20px" }}>🔥 See our latest roasts</Link>
     </div>
   );
 }
-
-export default Home;
